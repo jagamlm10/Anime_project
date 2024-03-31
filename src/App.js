abbc62c4d84c4703662c414578b7c89dd3652f55
Abhijat Bhaumik <abhijatbhaumik@gmail.com>
2024-03-31 23:40:08 +0530

    added navbar file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material';
-
+import Navbar from './components/Navbar';
 
 
 const app = () => {
   <BrowserRouter>
     <Box>
+      <Navbar />
       <Routes>
         
       </Routes>
