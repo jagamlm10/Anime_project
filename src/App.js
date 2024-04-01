b30cc1c55e5340d724022b0807c2c471730eda08
Abhijat Bhaumik <abhijatbhaumik@gmail.com>
2024-04-01 16:39:58 +0530

    added feed file and index file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material';
-import Navbar from './components/Navbar';
+import {Navbar, Feed, GenreDetail, AnimeDetail, SearchPage, TopAnime} from './components';
 
 
 const app = () => {
   <BrowserRouter>
-    <Box>
+    <Box sx={{background: '#66095e'}}>
       <Navbar />
       <Routes>
-        
+        <Route path='/' exact element = {<Feed />} />
+        <Route path='/anime/:id'  element = {<AnimeDetail />}/>
+        <Route path='/genre/:id'  element = {<GenreDetail />}/>
+        <Route path='/topanime' element = {<TopAnime />} />
+        <Route path='/search/:searchTerm'  element = {<SearchPage />}/>
       </Routes>
     </Box>
   </BrowserRouter>
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar} from './Navbar';
+export {default as AnimeDetail} from './AnimeDetail';
+export {default as GenreDetail} from './GenreDetail';
+export {default as SearchPage} from './SearchPage';
+export {default as TopAnime} from './TopAnime';
+export {default as Feed} from './Feed';
